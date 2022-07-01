63f76926ea7c3f30e1071e48774e4f6a30c1030e
Joy Chakraborty <chakrabortyjoy638@gmail.com>
2022-07-01 08:30:21 +0530

    Day 1 Code Upload

---
+++ Day1/ver.java
@@ -0,0 +1,10 @@
+class ver{
+public static void main(String[] args){
+int ver;
+ver=0;
+ver=ver+5;
+ver=ver-3;
+ver=ver-1+5;
+System.out.println(ver);
+}
+}
---
+++ Day1/yesiamjc.java
@@ -0,0 +1,5 @@
+class yesiamjc{
+public static void main(String[] args){
+System.out.println("I will do great in life irrespective of circumstances");
+}
+}
