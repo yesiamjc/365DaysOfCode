5504d81ef1a4d936558634f2bd0da65f0e25c897
Joy Chakraborty <chakrabortyjoy638@gmail.com>
2022-07-04 13:14:06 +0530

    Day 4 code upload

---
+++ Day4/boll.java
@@ -0,0 +1,8 @@
+public class boll{
+public static void main(String[] args){
+boolean isRaining=true;
+if(isRaining){
+System.out.println("Its raining so put windsheild wipers on");
+}
+}
+}
---
+++ Day4/trafic.java
@@ -0,0 +1,13 @@
+public class trafic{
+public static void main(String[] args){
+boolean isLightGreen=false;
+boolean isLightYellow=true;
+if(isLightGreen){
+System.out.println("You can drive.");
+}else if(isLightYellow){
+System.out.println("Slow down your car.");
+}else{
+System.out.println("Stop the car.");
+}
+}
+}
