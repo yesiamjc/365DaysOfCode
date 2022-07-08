7afb9ad6c693427ba6733f69f459d515f485cd07
Joy Chakraborty <chakrabortyjoy638@gmail.com>
2022-07-08 21:15:28 +0530

    Day8 Code Upload

---
+++ Day8/day8.java
@@ -0,0 +1,16 @@
+// Q3
+public class day8{
+public static void main(String[] args){
+boolean canSeePlayer=false;
+boolean isPoweredUp = true;
+if (canSeePlayer){
+if(!isPoweredUp){
+System.out.println("Attack!");
+}else{
+System.out.println("Run away!");
+}
+}else{
+System.out.println("Wander.");
+}
+}
+}
