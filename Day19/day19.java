801006debcf30b8c626b87bbd077841c12f499f9
Joy Chakraborty <chakrabortyjoy638@gmail.com>
2022-07-19 22:17:03 +0530

    Day 19 code upload

---
+++ Day19/day19.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class day19{
+public static void main(String[] args){
+int n;
+System.out.println("Enter number : ");
+Scanner sk=new Scanner(System.in);
+n=sk.nextInt();
+System.out.println("Entered number is "+n);
+}
+}
