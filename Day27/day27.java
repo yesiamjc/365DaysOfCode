25370f1a3d0e969ad22eace49a5ed491d3ff3b6d
Joy Chakraborty <chakrabortyjoy638@gmail.com>
2022-07-27 23:33:20 +0530

    Day 27 Code Upload

---
+++ Day27/day27.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class input{
+Scanner sc=new Scanner(System.in);
+String an=sc.nextLine();
+}
+public class day27{
+public static void main(String[] args){
+String b;
+input i=new input();
+b=i.an;
+System.out.println(b);
+}
+}
