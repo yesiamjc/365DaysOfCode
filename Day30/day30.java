4acf7c24a9ca05e7bb7b3ff0db7e77ab9bcecdc0
Joy Chakraborty <chakrabortyjoy638@gmail.com>
2022-07-30 20:40:38 +0530

    Day 30 Code Upload

---
+++ Day30/day30.java
@@ -0,0 +1,13 @@
+class yoi{
+int a;
+public void mN(){
+int b=0;
+System.out.println(a+"\n"+b);
+}
+}
+public class day30{
+public static void main(String[] args){
+yoi y = new yoi();
+y.mN();
+}
+}
