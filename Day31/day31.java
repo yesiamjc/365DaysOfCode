b8baeee3e156c7eff32edc01527b225528ee98e1
Joy Chakraborty <chakrabortyjoy638@gmail.com>
2022-07-31 21:33:08 +0530

    Day 31 Code Upload

---
+++ Day31/day31.java
@@ -0,0 +1,13 @@
+class  chai{
+private int cup,glass;
+public void lkr()
+{
+System.out.println("OP CHAI !!!");
+}
+}
+public class day31{
+public static void main(String[] args){
+chai c=new chai();
+c.lkr();
+}
+}
