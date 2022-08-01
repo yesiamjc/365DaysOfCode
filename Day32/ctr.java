57ac403193cad7229b71caeddbca4ce69769b8f2
Joy Chakraborty <chakrabortyjoy638@gmail.com>
2022-08-01 23:31:05 +0530

    Day 32 Code Upload

---
+++ Day32/ctr.java
@@ -0,0 +1,8 @@
+class ctr{
+static{
+System.out.println("hello");
+}
+public static void main(String[] args){
+
+}
+}
---
+++ Day32/day32.java
@@ -0,0 +1,13 @@
+class cont{
+int x;
+cont(){
+x=10;
+}
+}
+class day32{
+public static void main(String[] args){
+cont c=new cont();
+int b=c.x;
+System.out.println(b);
+}
+}
